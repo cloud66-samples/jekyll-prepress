b94363c79f8439fe3d4890ceb70a339bef1faffc
Khash Sajadi <khash@sajadi.co.uk>
2022-04-22 13:43:15 -0700

    init
    Signed-off-by: Khash Sajadi <khash@sajadi.co.uk>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Prepress Sample
+
+This is a sample Jekyll project to be deployed with [Cloud 66 Prepress](https://cloud66.com/frameworks/static-sites).
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.html',
+    './*.html'    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
